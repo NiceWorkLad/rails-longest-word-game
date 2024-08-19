e6f6121075310d1bba5bb4d9b18aa03fa326bacb
louis whittleadams <louiswhittle7d1@gmail.com>
2024-08-19 15:55:14 +0100

    having probelms with word validation

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,6 +4,16 @@ class GamesController < ApplicationController
   end
 
   def score
-    raise
+    @word = params[:word]
+    word_array = @word.split('')
+    can_be_made = word_array.each do |letter|
+      unless @letters.include?(letter)
+        return false
+      end
+    end
+    word_counts = if can_be_made && is_a_word
+    end
+    wrong_msg = "Sorry but #{word} can't be built out of #{@letters}"
+    {"message":@word,"endpoints":["https://dictionary.lewagon.com/:word","https://dictionary.lewagon.com/autocomplete/:stem"],"total_api_hits":97485,"words_found":22889,"autocomplete_hits":27391}
   end
 end
